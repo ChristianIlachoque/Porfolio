d2b05c370acd25eaf9a276fb330130446fc79315
ChristianIlachoque <cilachoque@unsa.edu.pe>
2024-06-27 13:42:29 -0500

    update

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -104,7 +104,7 @@ export const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
-                <h4>Express</h4>
+                <h4>Spring Boot</h4>
                 <small className="text-light">Intermedio</small>
               </div>
             </article>
